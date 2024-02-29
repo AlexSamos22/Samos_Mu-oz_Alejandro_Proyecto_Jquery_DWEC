4b7c8dcb62df9762810ac972aa830a18d62ad0b0
AlexSamos22 <asammun767@g.educaand.es>
2024-02-29 17:09:05 +0100

    arreglo y uso de las 2 APIS para sacar razas coincidentes

--- fuente/js/main.js
+++ fuente/js/main.js
@@ -14,14 +14,14 @@ $(document).ready(function() {
 
     lista.click(() =>{
         vista = "lista";
-        obtenerDatosGatos(offset).then(function(datosGatos) {
+        obtenerDatosGatos().then(function(datosGatos) {
             renderizarLista(datosGatos);
         });
     });
 
     tabla.click(() =>{
         vista = "tabla";
-        obtenerDatosGatos(offset).then(function(datosGatos) {
+        obtenerDatosGatos().then(function(datosGatos) {
             renderizarTabla(datosGatos);
         });
     });
@@ -31,12 +31,12 @@ $(document).ready(function() {
         tipoOrden = "ASC";
         if (vista == "lista") {
             ul.empty();
-            obtenerDatosGatos(offset).then(function(datosGatos) {
+            obtenerDatosGatos().then(function(datosGatos) {
                 renderizarLista(datosGatos);
             });
         }else{
             cuerpo.empty();
-            obtenerDatosGatos(offset).then(function(datosGatos) {
+            obtenerDatosGatos().then(function(datosGatos) {
                 renderizarTabla(datosGatos);
             });
         }
@@ -47,47 +47,155 @@ $(document).ready(function() {
         tipoOrden = "DESC";
         if (vista == "lista") {
             ul.empty();
-            obtenerDatosGatos(offset).then(function(datosGatos) {
+            obtenerDatosGatos().then(function(datosGatos) {
                 renderizarLista(datosGatos);
             });
         }else{
             cuerpo.empty();
-            obtenerDatosGatos(offset).then(function(datosGatos) {
+            obtenerDatosGatos().then(function(datosGatos) {
                 renderizarTabla(datosGatos);
             });
         }
        
     })
 
-    async function obtenerDatosGatos(offset) {
+    async function obtenerDatosFacts() {
         try {
             let respuesta;
     
             // Construye la URL para obtener más datos de gatos
-            let url = "https://api.thecatapi.com/v1/images/search";
-            let parametrosConsulta = {
-                limit: 10,
-                api_key: "live_vIcm09jTwDDE89WlD2S9JAEn5wz1laQkoJuiuHGcvAUTc3noy8MwpyhL0m6oBpDO",
-                mime_types: "jpg",
-                format: "json",
-                has_breeds: true,
-                order: "RAND",
-                page: offset
-            };
-
-            if (orden) {
-                parametrosConsulta.order = tipoOrden;
-            }
+            let url = "https://catfact.ninja/breeds";
     
             // Realiza la solicitud AJAX utilizando jQuery
             respuesta = await $.ajax({
                 url: url,
                 method: "GET",
-                data: parametrosConsulta,
                 dataType: "json"
             });
     
-            return respuesta;
+            // Verifica si la respuesta contiene datos
+            if (respuesta && respuesta.data && respuesta.data.length > 0) {
+                // Crea un array para almacenar los nombres de las razas
+                let nombresRazas = [];
+    
+                // Itera sobre los datos y guarda los nombres de las razas en el array
+                respuesta.data.forEach((item) => {
+                    nombresRazas.push(item.breed);
+                });
+    
+
+    
+                // Retorna el array con los nombres de las razas
+                return nombresRazas;
+            } else {
+                console.log('No se encontraron datos de razas de gatos.');
+                return [];
+            }
+        } catch (error) {
+            console.error('Error al obtener datos de gatos:', error);
+            throw error;
+        }
+    }
+
+    async function obtenerBreeds() {
+        try {
+            // URL para obtener los 25 primeros breeds
+            let url = "https://api.thecatapi.com/v1/breeds?limit=25";
+    
+            // Realiza la solicitud AJAX utilizando jQuery
+            let respuesta = await $.ajax({
+                url: url,
+                method: "GET",
+                dataType: "json"
+            });
+    
+            // Array para almacenar los breeds con id y name
+            let breeds = [];
+    
+            // Itera sobre la respuesta y almacena los breeds con id y name en el array
+            respuesta.forEach((breed) => {
+                breeds.push({
+                    id: breed.id,
+                    name: breed.name
+                });
+            });
+            return breeds;
+        } catch (error) {
+            console.error('Error al obtener breeds:', error);
+            throw error;
+        }
+    }
+
+    async function obtenerIdsRazasCoincidentes() {
+        try {
+            // Obtener los breeds y los facts
+            let breeds = await obtenerBreeds();
+            let facts = await obtenerDatosFacts();
+    
+            // Array para almacenar los IDs de las razas coincidentes
+            let idsRazasCoincidentes = [];
+    
+            // Recorre los facts y los breeds para buscar coincidencias
+            facts.forEach((fact) => {
+                breeds.forEach((breed) => {
+                    // Compara los nombres de las razas en ambos arrays
+                    if (fact === breed.name) {
+                        // Si hay coincidencia, guarda el ID de la raza
+                        idsRazasCoincidentes.push(breed.id);
+                    }
+                });
+            });
+    
+            return idsRazasCoincidentes;
+        } catch (error) {
+            console.error('Error al obtener IDs de razas coincidentes:', error);
+            throw error;
+        }
+    }
+
+    async function obtenerDatosGatos() {
+        try {
+            let razas = await obtenerIdsRazasCoincidentes();
+            let respuestaRazas = []; // Array para almacenar las respuestas de las razas
+    
+            // Iterar sobre cada ID de raza
+            for (let i = 0; i < razas.length; i++) {
+                let url = `https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${razas[i]}&api_key=live_vIcm09jTwDDE89WlD2S9JAEn5wz1laQkoJuiuHGcvAUTc3noy8MwpyhL0m6oBpDO`;
+                
+                let parametrosConsulta = {
+                    mime_types: "jpg",
+                    format: "json",
+                    order: "RAND",
+                    page: offset
+                };
+
+                // Realizar la solicitud AJAX utilizando jQuery
+                let respuestaRaza = await $.ajax({
+                    url: url,
+                    method: "GET",
+                    data: parametrosConsulta,
+                    dataType: "json"
+                });
+    
+                // Agregar la respuesta de la raza al array de respuestas
+                respuestaRazas.push(respuestaRaza);
+            }
+
+            respuestaRazas.sort((a, b) => {
+                let nombreA = a[0].breeds[0].name.toLowerCase();
+                let nombreB = b[0].breeds[0].name.toLowerCase();
+                if (tipoOrden === "ASC") {
+                    return nombreA.localeCompare(nombreB);
+                } else if (tipoOrden === "DESC") {
+                    return nombreB.localeCompare(nombreA);
+                } else {
+                    return 0;
+                }
+            });
+    
+            // Imprimir las respuestas de las razas
+            return respuestaRazas;
+    
         } catch (error) {
             console.error('Error al obtener datos de gatos:', error);
             throw error;
@@ -95,11 +203,12 @@ $(document).ready(function() {
     }
     
     // Función para generar una fila con los datos de un gato
-    function generarFilaTablaGato(gato) {
+    function generarFilaTablaGato(gatoData) {
+        var gato = gatoData[0]; // Obtener el objeto de gato del array
         var fila = $("<tr>");
         var imagen = $("<img>").attr("src", gato.url).attr("alt", "Imagen de gato").attr("width", "30%");
-        var raza = gato.breeds.length > 0 ? gato.breeds[0].name : "Desconocida";
-        var nombre = gato.breeds.length > 0 ? gato.breeds[0].id : "Desconocido";
+        var raza = gato.breeds[0].name;
+        var nombre = gato.breeds[0].id;
     
         fila.append($("<td>").append(imagen));
         fila.append($("<td>").text(nombre));
@@ -107,12 +216,13 @@ $(document).ready(function() {
     
         return fila;
     }
-
-    function generarElementosListaGato(gato) {
+    
+    function generarElementosListaGato(gatoData) {
+        var gato = gatoData[0]; // Obtener el objeto de gato del array
         var li = $("<li>");
         var imagen = $("<img>").attr("src", gato.url).attr("alt", "Imagen de gato").attr("width", "30%");
-        var raza = gato.breeds.length > 0 ? gato.breeds[0].name : "Desconocida";
-        var nombre = gato.breeds.length > 0 ? gato.breeds[0].id : "Desconocido";
+        var raza = gato.breeds[0].name;
+        var nombre = gato.breeds[0].id;
     
         li.append($("<p>").append(imagen));
         li.append($("<p>").text(nombre));
@@ -175,11 +285,11 @@ $(document).ready(function() {
             offset++; // Incrementar el offset para la próxima paginación
             // Si se llega al final de la página, cargar más datos de gatos
             if (vista == "lista") {
-                obtenerDatosGatos(offset).then(function(datosGatos) {
+                obtenerDatosGatos().then(function(datosGatos) {
                     añadirElementoALista(datosGatos)
                  });
             }else{
-                obtenerDatosGatos(offset).then(function(datosGatos) {
+                obtenerDatosGatos().then(function(datosGatos) {
                     añadirFilaTabla(datosGatos)
                  });
             }
@@ -187,7 +297,7 @@ $(document).ready(function() {
     });
     
     // Cargar datos de gatos inicialmente
-    obtenerDatosGatos(offset).then(function(datosGatos) {
+    obtenerDatosGatos().then(function(datosGatos) {
         renderizarLista(datosGatos);
      });
     
